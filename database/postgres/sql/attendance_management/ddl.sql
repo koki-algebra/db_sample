430dd33bbbe984e44f4a744c0d4cf2ca8faa57f6
koki-algebra <yotsuba00040@gmail.com>
2023-11-10 22:33:46 +0900

    add constraints

--- database/postgres/sql/attendance_management/ddl.sql
+++ database/postgres/sql/attendance_management/ddl.sql
@@ -13,7 +13,13 @@ CREATE TABLE attendances (
 	)
 );
 
+-- 同日に二度出勤できないようにする
 CREATE UNIQUE INDEX uq_attendances_01 ON attendances (CAST(started_at AS DATE), user_id);
+
+-- 退勤前に次の出勤ができないようにする
+CREATE UNIQUE INDEX uq_attendances_02 ON attendances (user_id)
+WHERE
+	ended_at IS NULL;
 
 -- 休憩
 CREATE TABLE breaks (
@@ -26,3 +32,8 @@ CREATE TABLE breaks (
 		OR (started_at <= ended_at)
 	)
 );
+
+-- 休憩終了前に休憩開始できないようにする
+CREATE UNIQUE INDEX uq_breaks_01 ON breaks (attendance_id)
+WHERE
+	ended_at IS NULL;
