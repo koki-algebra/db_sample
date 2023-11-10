18fb96cfe5a3e1a71938638364e52fdfa3b150c4
koki-algebra <yotsuba00040@gmail.com>
2023-11-10 14:57:12 +0900

    update attendance management

--- database/postgres/sql/attendance_management/ddl.sql
+++ database/postgres/sql/attendance_management/ddl.sql
@@ -1,42 +1,28 @@
 -- ユーザー
 CREATE TABLE users (user_id SERIAL PRIMARY KEY, username TEXT);
 
--- 出勤開始
-CREATE TABLE start_works (
-	start_work_id SERIAL PRIMARY KEY,
+-- 勤怠
+CREATE TABLE attendances (
+	attendance_id SERIAL PRIMARY KEY,
 	user_id INTEGER NOT NULL REFERENCES users(user_id),
-	started_at TIMESTAMP
+	started_at TIMESTAMP NOT NULL,
+	ended_at TIMESTAMP,
+	CONSTRAINT check_attendances_01 CHECK (
+		ended_at IS NULL
+		OR started_at <= ended_at
+	)
 );
 
--- 同日に二度出勤できないようにする
-CREATE UNIQUE INDEX uq_start_works_01 ON start_works (CAST(started_at AS DATE), user_id);
+CREATE UNIQUE INDEX uq_attendances_01 ON attendances (CAST(started_at AS DATE), user_id);
 
--- 出勤終了
-CREATE TABLE end_works (
-	end_work_id SERIAL PRIMARY KEY,
-	user_id INTEGER NOT NULL REFERENCES users(user_id),
-	ended_at TIMESTAMP
+-- 休憩
+CREATE TABLE breaks (
+	break_id SERIAL PRIMARY KEY,
+	attendance_id INTEGER NOT NULL REFERENCES attendances(attendance_id),
+	started_at TIMESTAMP NOT NULL,
+	ended_at TIMESTAMP,
+	CONSTRAINT check_breaks_01 CHECK (
+		ended_at IS NULL
+		OR (started_at <= ended_at)
+	)
 );
-
--- 同日に二度退勤できないようにする
-CREATE UNIQUE INDEX uq_end_works_01 ON end_works (CAST(ended_at AS DATE), user_id);
-
--- 休憩開始
-CREATE TABLE start_breaks (
-	start_break_id SERIAL PRIMARY KEY,
-	user_id INTEGER NOT NULL REFERENCES users(user_id),
-	started_at TIMESTAMP
-);
-
--- 同日に二度休憩開始できないようにする
-CREATE UNIQUE INDEX uq_start_breaks_01 ON start_breaks (CAST(started_at AS DATE), user_id);
-
--- 休憩終了
-CREATE TABLE end_breaks (
-	end_break_id SERIAL PRIMARY KEY,
-	user_id INTEGER NOT NULL REFERENCES users(user_id),
-	ended_at TIMESTAMP
-);
-
--- 同日に二度休憩終了できないようにする
-CREATE UNIQUE INDEX uq_end_breaks_01 ON end_breaks (CAST(ended_at AS DATE), user_id);
--- database/postgres/sql/attendance_management/query.sql
+++ database/postgres/sql/attendance_management/query.sql
@@ -1,31 +1,14 @@
 -- 勤怠履歴を取得
-WITH attendances AS (
-	SELECT
-		CAST(sw.started_at AS DATE) AS date,
-		sw.user_id,
-		sw.started_at AS work_started_at,
-		sb.started_at AS break_started_at,
-		eb.ended_at AS break_ended_at,
-		ew.ended_at AS work_ended_at
-	FROM
-		start_works AS sw
-		LEFT JOIN end_works AS ew ON CAST(sw.started_at AS DATE) = CAST(ew.ended_at AS DATE)
-		AND sw.user_id = ew.user_id
-		LEFT JOIN start_breaks AS sb ON CAST(sw.started_at AS DATE) = CAST(sb.started_at AS DATE)
-		AND sw.user_id = sb.user_id
-		LEFT JOIN end_breaks AS eb ON CAST(sw.started_at AS DATE) = CAST(eb.ended_at AS DATE)
-		AND sw.user_id = eb.user_id
-)
 SELECT
-	date,
+	CAST(a.started_at AS DATE) AS date,
+	attendance_id,
 	u.user_id,
 	username,
-	work_started_at,
-	break_started_at,
-	break_ended_at,
-	work_ended_at
+	a.started_at AS work_started_at,
+	a.ended_at AS work_ended_at,
+	b.started_at AS break_started_at,
+	b.ended_at AS break_ended_at
 FROM
-	users AS u
-	JOIN attendances USING(user_id)
-ORDER BY
-	date ASC;
+	attendances AS a
+	INNER JOIN users AS u USING(user_id)
+	LEFT JOIN breaks AS b USING(attendance_id);
